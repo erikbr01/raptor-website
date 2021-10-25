d455a25147349e5036c299dc7d70f049fc481360
Erik Bauer <erik.hamburg@gmx.de>
2021-10-25 18:09:53 +0200

    Fixed transparent navbar with background image

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
-$(document).ready(function() {
-    $(window).scroll(function() {    
+$(document).ready(function () {
+    let classes = $('.navbar')[0].className.split(' ')
+    let changeTheme = classes.includes('navbar-dark');
+    $(window).scroll(function () {
         var scroll = $(window).scrollTop();
         if (scroll >= 180) {
             $(".navbar").addClass("bg-white");
+
+            if (changeTheme) {
+                $(".navbar").addClass("navbar-light");
+                $(".navbar").removeClass("navbar-dark");
+
+            }
         } else {
-          $(".navbar").removeClass("bg-white");
+            $(".navbar").removeClass("bg-white");
+            if (changeTheme) {
+                $(".navbar").removeClass("navbar-light");
+                $(".navbar").addClass("navbar-dark");
+
+            }
         }
-    }); 
+    });
 });
-    
-    
