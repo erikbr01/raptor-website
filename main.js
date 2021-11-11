fc6631f4c7ccefe5eaec893b40d10894587fa602
Erik Bauer <erik.hamburg@gmx.de>
2021-11-11 15:29:30 +0100

    Fixed the buggy navbar behaviour for md screens by making dropdown nav opaque

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
+let scrollBorder = 200;
+var originalIsWhite = false;
+
+
 $(document).ready(function () {
+
     let classes = $('.navbar')[0].className.split(' ')
     let changeTheme = classes.includes('navbar-dark');
+    originalIsWhite = changeTheme;
+
     $(window).scroll(function () {
         var scroll = $(window).scrollTop();
-        if (scroll >= 180) {
+        if (scroll >= scrollBorder) {
             $(".navbar").addClass("bg-white");
 
             if (changeTheme) {
@@ -21,3 +28,29 @@ $(document).ready(function () {
         }
     });
 });
+
+var navbarIsExpanded = false;
+
+
+function onNavbarExpansion() {
+
+    let classes = $('.navbar')[0].className.split(' ');
+
+    console.log(classes);
+    if (!navbarIsExpanded) {
+        $(".navbar").addClass("bg-white");
+        if (originalIsWhite) {
+            $(".navbar").addClass("navbar-light");
+            $(".navbar").removeClass("navbar-dark");
+        }
+    } else if ($(window).scrollTop() < scrollBorder) {
+        $(".navbar").removeClass("bg-white");
+        if (originalIsWhite) {
+            $(".navbar").addClass("navbar-dark");
+            $(".navbar").removeClass("navbar-light");
+        }
+    } else {
+
+    }
+    navbarIsExpanded = !navbarIsExpanded;
+}
