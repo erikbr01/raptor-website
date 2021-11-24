167be6ccbcd38eac0dac49d9d6b4b49e6e3061d9
Erik Bauer <erik.hamburg@gmx.de>
2021-11-24 16:16:27 +0100

    js for navbar is running, rough redesign with new hero title placement on landing page

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 // determines the scroll length until the navbar becomes opaque
 let scrollBorder = 200;
 
-// state variable that determines whether the navbar is navbar-dark or navbar-light by default when loading the page
-var originalIsWhite = false;
+
 
 // state variable that determines whether the navbar is currently in the expanded state or not
 // expanded in this case refers to the state on mobile devices with the collapsible navbar (hamburger button)
@@ -11,31 +10,30 @@ var navbarIsExpanded = false;
 $(document).ready(function () {
     // navbar classes
     let classes = $('.navbar')[0].className.split(' ')
-
+    
     // this may seem like a duplicate variable, but it's necessary for immediate updates of the text colour when the state changes
-    let changeTheme = classes.includes('navbar-dark');
-    originalIsWhite = changeTheme;
 
 
     // on scroll, update the navbar theme depending on how far the user has scrolled
     // change the background to make the navbar opaque and adjust the text to be readable depending on the text colour we start with
     $(window).scroll(function () {
+        console.log(classes);
         if (!navbarIsExpanded) {
             var scroll = $(window).scrollTop();
             if (scroll >= scrollBorder) {
-                $(".navbar").addClass("bg-white");
+                $(".navbar").addClass("bg-black");
 
                 // change text colour
-                if (changeTheme) {
-                    $(".navbar").addClass("navbar-light");
-                    $(".navbar").removeClass("navbar-dark");
-                }
+                // if (changeTheme) {
+                //     $(".navbar").addClass("navbar-light");
+                //     $(".navbar").removeClass("navbar-dark");
+                // }
             } else {
-                $(".navbar").removeClass("bg-white");
-                if (changeTheme) {
-                    $(".navbar").removeClass("navbar-light");
-                    $(".navbar").addClass("navbar-dark");
-                }
+                $(".navbar").removeClass("bg-black");
+                // if (changeTheme) {
+                //     $(".navbar").removeClass("navbar-light");
+                //     $(".navbar").addClass("navbar-dark");
+                // }
             }
         }
     });
@@ -45,19 +43,30 @@ $(document).ready(function () {
 // callback for the onclick event from the hamburger button on the mobile navbar
 // make navbar opaque when expanded to prevent any issues with readability and adjust text colour
 function onNavbarExpansion() {
+    console.log($('.navbar')[0].className.split(' '));
     if (!navbarIsExpanded) {
         $(".navbar").addClass("bg-white");
-        if (originalIsWhite) {
-            $(".navbar").addClass("navbar-light");
-            $(".navbar").removeClass("navbar-dark");
-        }
-    // if the navbar is in the top region, it should be transparent again
+        $(".navbar").removeClass("navbar-dark");
+        $(".navbar").addClass("navbar-light");
+        // if (originalIsWhite) {
+        //     $(".navbar").addClass("navbar-light");
+        //     $(".navbar").removeClass("navbar-dark");
+        // }
+        // if the navbar is in the top region, it should be transparent again
     } else if ($(window).scrollTop() < scrollBorder) {
         $(".navbar").removeClass("bg-white");
-        if (originalIsWhite) {
-            $(".navbar").addClass("navbar-dark");
-            $(".navbar").removeClass("navbar-light");
-        }
+        $(".navbar").removeClass("bg-black");
+        $(".navbar").removeClass("navbar-light");
+        $(".navbar").addClass("navbar-dark");
+    } else {
+        $(".navbar").removeClass("bg-white");
+        $(".navbar").removeClass("navbar-light");
+        $(".navbar").addClass("navbar-dark");
+        $(".navbar").addClass("bg-black");
+        // if (originalIsWhite) {
+        //     $(".navbar").addClass("navbar-dark");
+        //     $(".navbar").removeClass("navbar-light");
+        // }
     }
     navbarIsExpanded = !navbarIsExpanded;
 }
